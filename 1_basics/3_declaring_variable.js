4e4f0562819b7f5ef5d2783e98284109339b2608
gratis-caelum <yaminma343@gmail.com>
2024-12-28 13:22:57 +0900

    Javascript 공부

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,40 @@
+/**
+ * Variable 선언
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let 
+ * 3) const
+ */
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/** 
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 선언 : 변수를 선언하는 것. (var name; : undefined (Default))
+ * 2) 할당 : 값을 넣어주는 것. (var name = '코드팩토리'; or name = '코드팩토리';)
+ */
+
+let girlFriend;
+console.log(girlFriend);
+
+// const girlFriend2; // 불가
---
+++ 1_basics/6_hoisting.js
@@ -0,0 +1,29 @@
+/**
+ * Hoisting
+ */
+
+console.log('Hello');
+console.log('World');
+
+// console.log(name);
+// var name = '코드팩토리';
+// console.log(name);
+
+/**
+ * Hoisting 은 무엇인가?
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ * let, const : 오류 발생 (하지만 Hoisting은 되고 있는 것.)
+ * var : 오류 발생하지 않음
+ */
+// var name;
+// console.log(name);
+// name = '코드팩토리';
+// console.log(name);
+
+// console.log(yuJin);
+// // let yuJin = '안유진';
+// console.log(yuJin);
+
+// console.log(yuJin);
+// const yuJin = '안유진';
